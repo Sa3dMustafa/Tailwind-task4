0873a1a555b59cab8e1acafc3e691b25bd2f2435
Sa3d0o <105570840+Sa3dMustafa@users.noreply.github.com>
2023-07-31 01:53:35 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,41 +1,35 @@
-let btnOpen =document.getElementById("open");
-            let btnClose =document.getElementById("close");
-            let navbar=document.getElementById("navbar");
+let btnOpen = document.getElementById("open");
+let btnClose = document.getElementById("close");
+let navbar = document.getElementById("navbar");
 
-            btnClose.onclick = function() {
-                navbar.classList.add("hidden");
-                this.classList.add("hidden");
-                btnOpen.classList.remove("hidden");
-            }
+btnClose.onclick = function () {
+    navbar.classList.add("hidden");
+    this.classList.add("hidden");
+    btnOpen.classList.remove("hidden");
+};
 
-            btnOpen.onclick =function() {
-                this.classList.add("hidden");
-                navbar.classList.remove("hidden");
-                btnClose.classList.remove("hidden");
-            }
+btnOpen.onclick = function () {
+    this.classList.add("hidden");
+    navbar.classList.remove("hidden");
+    btnClose.classList.remove("hidden");
+};
 
-            const optionElement = document.querySelector(".option");
-            const menuElement = optionElement.querySelector(".menu");
-            const upArrow = optionElement.querySelector(".up");
-            const downArrow = optionElement.querySelector(".down");
+const optionElement = document.querySelector(".option");
+const menuElement = optionElement.querySelector(".menu");
+const upArrow = optionElement.querySelector(".up");
+const downArrow = optionElement.querySelector(".down");
 
-            // Function to show the menu and switch arrows
-            function showMenu() {
-            menuElement.classList.remove("hidden");
-            upArrow.classList.remove("hidden");
-            downArrow.classList.add("hidden");
-            }
+function showMenu() {
+    menuElement.classList.remove("hidden");
+    upArrow.classList.remove("hidden");
+    downArrow.classList.add("hidden");
+}
 
-            // Function to hide the menu and switch arrows
-            function hideMenu() {
-            menuElement.classList.add("hidden");
-            upArrow.classList.add("hidden");
-            downArrow.classList.remove("hidden");
-            }
+function hideMenu() {
+    menuElement.classList.add("hidden");
+    upArrow.classList.add("hidden");
+    downArrow.classList.remove("hidden");
+}
 
-            // Initial state: menu and up arrow hidden, down arrow visible
-            hideMenu();
-
-            // Add click event listener to the arrows
-            upArrow.onclick = hideMenu;
-            downArrow.onclick = showMenu;
+upArrow.onclick = hideMenu;
+downArrow.onclick = showMenu;
